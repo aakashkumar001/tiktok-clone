ee89fe0cb0a1f4fe0318366e1075a3edcf837da9
aakash <aspakash000@gmail.com>
2023-10-10 13:32:51 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    //Add a rule to handle the canvas.node binary module
+    config.module.rules.push({ test: /\.node$/, use: "raw-loader" })
 
-module.exports = nextConfig
+    //exclude canvas from being processed by next.js in the browser
+    if (!isServer) config.externals.push('canvas');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
